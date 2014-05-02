3a21fd1cc836724a65645c529358c04cae7aa709
Thibault Jouan <tj@a13.fr>
2014-05-02 16:16:04 +0000

    Update rspec-rails generator config

--- config/initializers/generators.rb
+++ config/initializers/generators.rb
@@ -2,4 +2,10 @@ Rails.application.config.generators do |g|
   g.helper      false
   g.javascripts false
   g.stylesheets false
+
+  g.test_framework :rspec,
+    controller_specs: false,
+    request_specs: false,
+    routing_specs: false,
+    view_specs: false
 end
