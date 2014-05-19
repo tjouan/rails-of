d7d4e25292fcf30a53c87904a061cc42fa8fc614
Thibault Jouan <tj@a13.fr>
2014-05-19 11:59:45 +0000

    Fix fixture path in data files file spec

--- spec/features/data_files/file_spec.rb
+++ spec/features/data_files/file_spec.rb
@@ -5,14 +5,11 @@ feature 'Data files file' do
     visit new_data_file_path
 
     fill_in 'Label', with: 'some file'
-    attach_file 'Fichier', File.expand_path(fixture_path, 'sample_0.csv').to_s
+    attach_file 'Fichier', File.join(fixture_path, 'sample_0.csv').to_s
     click_button 'Enregistrer'
     click_link 'some file'
 
-    # FIXME: capybara won't set the correct content_type and file_name will be
-    # hardcoded to `fixtures'
-    # https://github.com/jnicklas/capybara/issues/1236
-    #expect(page.body).to include 'text/csv'
-    expect(page.body).to include 'fixtures'
+    expect(page.body).to include 'sample_0.csv'
+    expect(page.body).to include 'text/csv'
   end
 end
