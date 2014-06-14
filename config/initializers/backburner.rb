26a0cd78bb4f34db6873b509af8ad1d5c908f615
Thibault Jouan <tj@a13.fr>
2014-06-14 12:20:28 +0000

    Implement worker script

--- config/initializers/backburner.rb
+++ config/initializers/backburner.rb
@@ -1,6 +1,8 @@
 Backburner.configure do |config|
+  log_destination = Rails.env.production? ? 'log/worker.log' : $stdout
+
   config.beanstalk_url  = 'beanstalk://127.0.0.1'
   config.tube_namespace = 'opti'
-  config.logger         = Logger.new($stdout)
+  config.logger         = Logger.new(log_destination)
   config.on_error       = lambda { |e| $stderr.puts e }
 end
