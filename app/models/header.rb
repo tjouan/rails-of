56c234b4d9d3986812f433582a6a84961a6e030e
Thibault Jouan <tj@a13.fr>
2014-06-18 20:41:01 +0000

    Add long text header type

--- app/models/header.rb
+++ app/models/header.rb
@@ -1,9 +1,10 @@
 class Header < ActiveRecord::Base
   TYPES = {
-    text:   'texte',
-    int:    'entier',
-    float:  'flottant',
-    date:   'date'
+    text:     'texte',
+    longtext: 'texte long',
+    int:      'entier',
+    float:    'flottant',
+    date:     'date'
   }.freeze
 
   # in order to use type as a column name.
