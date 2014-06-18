4f65fa4cbb0b6a9f193ff1b064f12e1769dd5aa9
Thibault Jouan <tj@a13.fr>
2014-06-18 00:05:47 +0000

    Refactor spec factories

--- spec/factories.rb
+++ spec/factories.rb
@@ -5,22 +5,16 @@ FactoryGirl.define do
     name 'name'
     type :text
   end
-end
 
-FactoryGirl.define do
   factory :operation do
     name 'GeoScore'
   end
-end
 
-FactoryGirl.define do
   factory :source do
     label 'some file'
     file  { fixture_file_upload 'spec/fixtures/3col_header.csv', 'text/csv' }
   end
-end
 
-FactoryGirl.define do
   factory :work do
     operation
     source
