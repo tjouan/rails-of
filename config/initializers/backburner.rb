eae42bb2d0afa8d642d482b6de31c3a77c6f3b2f
Thibault Jouan <tj@a13.fr>
2014-06-18 02:14:43 +0000

    WiP: Fix duplicate output in worker log

--- config/initializers/backburner.rb
+++ config/initializers/backburner.rb
@@ -4,5 +4,4 @@ Backburner.configure do |config|
   config.beanstalk_url  = 'beanstalk://127.0.0.1'
   config.tube_namespace = 'opti'
   config.logger         = Logger.new(log_destination)
-  config.on_error       = lambda { |e| $stderr.puts e }
 end
