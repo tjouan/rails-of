880a93097512332b74a68affcde41a6ba8223919
Thibault Jouan <tj@a13.fr>
2014-06-18 02:27:39 +0000

    Increase worker timeout from 120s to 900s

--- config/initializers/backburner.rb
+++ config/initializers/backburner.rb
@@ -1,7 +1,8 @@
 Backburner.configure do |config|
   log_destination = Rails.env.production? ? 'log/worker.log' : $stdout
 
-  config.beanstalk_url  = 'beanstalk://127.0.0.1'
-  config.tube_namespace = 'opti'
-  config.logger         = Logger.new(log_destination)
+  config.beanstalk_url    = 'beanstalk://127.0.0.1'
+  config.tube_namespace   = 'opti'
+  config.logger           = Logger.new(log_destination)
+  config.respond_timeout  = 3600 / 4
 end
