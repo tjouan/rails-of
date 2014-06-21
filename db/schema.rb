c92f7bddff747fde8660ac1c906d64501f77983b
Thibault Jouan <tj@a13.fr>
2014-06-21 18:57:04 +0000

    Revise DB schema usage and versionning:
    * Unignore ruby schema from version control;
    * Move schema configuration in main config;
    * Add ruby DB schema.

---
+++ db/schema.rb
@@ -0,0 +1,56 @@
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20140620201555) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "headers", force: true do |t|
+    t.integer  "source_id",  null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "name",       null: false
+    t.integer  "type",       null: false
+    t.integer  "position",   null: false
+  end
+
+  create_table "operations", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "name",       null: false
+  end
+
+  create_table "sources", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "label",                       null: false
+    t.string   "description"
+    t.string   "sha256",                      null: false
+    t.string   "file_name"
+    t.string   "mime_type"
+    t.string   "charset"
+    t.boolean  "file_header", default: false, null: false
+  end
+
+  create_table "works", force: true do |t|
+    t.integer  "operation_id", null: false
+    t.integer  "source_id",    null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.datetime "started_at"
+    t.datetime "processed_at"
+    t.string   "parameters",   null: false, array: true
+  end
+
+end
