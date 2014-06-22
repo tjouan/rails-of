d911838258bffb11c8e87fc1aec79c2e4bda72f3
Thibault Jouan <tj@a13.fr>
2014-06-22 15:36:50 +0000

    Add missing validations on Work model

--- app/models/work.rb
+++ app/models/work.rb
@@ -2,6 +2,9 @@ class Work < ActiveRecord::Base
   belongs_to :operation
   belongs_to :source
 
+  validates :operation, presence: true
+  validates :source,    presence: true
+
   # FIXME: validates_presence_of won't work well with an empty array (returns
   # true), and there are no specific validation to test for nil.
   validates :parameters, length: { minimum: 0, allow_nil: false }
--- spec/models/work_spec.rb
+++ spec/models/work_spec.rb
@@ -7,6 +7,14 @@ describe Work do
     expect(work).to be_valid
   end
 
+  it 'validates presence of the operation' do
+    expect(work).to validate_presence_of :operation
+  end
+
+  it 'validates presence of the source' do
+    expect(work).to validate_presence_of :source
+  end
+
   it 'validates parameters existence' do
     expect(work).not_to allow_value(nil).for :parameters
   end
