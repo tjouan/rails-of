6bf284ddd9e20d88a3eddb815ed3b438db4d8b5f
Thibault Jouan <tj@a13.fr>
2014-08-03 13:43:14 +0000

    Sort configured routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,17 @@
 Rails.application.routes.draw do
   root 'home#index'
 
+  get '/dashboard' => 'dashboard#show'
+
+  resources :operations, only: [:index, :show]
+
   resources :sources do
     resource :headers, only: :edit
     resource :preview, only: :show
     resource :download, only: :show
   end
 
-  resources :operations, only: [:index, :show]
-
   resources :works, only: [:index, :show, :new, :create]
-
-  get '/dashboard' => 'dashboard#show'
 
   get '/404' => 'errors#not_found'
   get '/500' => 'errors#internal_error'
