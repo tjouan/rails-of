bb4c6a99fec20181508ffc893b5f59573fc4108d
Thibault Jouan <tj@a13.fr>
2014-08-16 22:30:18 +0000

    Fix sources crud specs

--- spec/features/sources/crud_spec.rb
+++ spec/features/sources/crud_spec.rb
@@ -3,15 +3,15 @@ require 'spec_helper'
 feature 'Sources CRUD' do
   include AcceptanceHelpers
 
-  background { sign_in }
+  background do
+    sign_in
+    create_source
+  end
 
   scenario 'destroys source' do
-    create :source, user: current_user
-    visit sources_path
-
     click_icon 'Delete'
 
     expect(current_path).to eq sources_path
-    expect(page).not_to have_content 'some file'
+    expect(page).not_to have_content 'mydata'
   end
 end
