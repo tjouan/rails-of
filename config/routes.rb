74a6cff17de66b4077135fa3c11daba680bc4ae6
Thibault Jouan <tj@a13.fr>
2014-08-18 11:17:45 +0000

    Sort routes lexically

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
   root 'home#index'
 
+  get '/404' => 'errors#not_found'
+  get '/500' => 'errors#internal_error'
+
   get '/dashboard' => 'dashboard#show'
 
   resources :operations, only: %i[index show]
+
+  resources :sessions, only: :create
+  get '/signin' => 'sessions#new'
 
   resources :sources do
     resource :headers, only: :edit
@@ -11,15 +17,9 @@ Rails.application.routes.draw do
     resource :download, only: :show
   end
 
-  resources :sessions, only: :create
-  get '/signin' => 'sessions#new'
-
   resources :users, only: %i[new create]
 
   resources :works, only: %i[index show new create]
-
-  get '/404' => 'errors#not_found'
-  get '/500' => 'errors#internal_error'
 
   namespace :admin do
     get '', to: 'dashboard#show'
