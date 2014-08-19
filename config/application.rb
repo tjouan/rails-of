7047371e9772baffff2a46bd46bfafd2514835d9
Thibault Jouan <tj@a13.fr>
2014-08-19 22:17:06 +0000

    Force HTTPS usage in production environment

--- config/application.rb
+++ config/application.rb
@@ -26,6 +26,8 @@ module OptiFront
 
     config.active_record.schema_format :sql
 
+    config.force_ssl = true if Rails.env.production?
+
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
