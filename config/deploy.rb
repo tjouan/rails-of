38f18f57e7aa020b9a18f6322a6ee463dd5aa76d
Thibault Jouan <tj@a13.fr>
2014-09-25 14:53:59 +0000

    Use producer/rails in deploy recipe

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,156 +1,18 @@
-require 'securerandom'
+require 'producer/rails'
 
 
-repository      = 'git:datacube/opti-front'
-app_path        = 'www/opti-front'
-www_workers     = 2
-queue_workers   = (env.target.include? 'prod') ? 2 : 1
+set :repository,      'git:datacube/opti-front'
+set :app_path,        'www/opti-front'
+set :app_mkdir,       %w[data data/sources]
 
-www_config_path = "#{app_path}/config/unicorn.rb"
-www_pid_path    = 'tmp/run/www.pid'
-www_sock_path   = 'tmp/run/www.sock'
+set :queue,           :backburner
+set :queue_workers,   (target.include? 'prod') ? 2 : 1
 
-source 'vendor/deploy/stdlib/fs'
-source 'vendor/deploy/stdlib/git'
-
-
-macro :bundle_install do
-  gemfile = "--gemfile #{app_path}/Gemfile"
-
-  condition { no_sh "bundle check #{gemfile}" }
-
-  sh "bundle install --without development test #{gemfile}"
-end
+set :www_server,      :unicorn
+set :www_workers,     2
+set :www_config_path, 'config/unicorn.rb'
+set :www_pid_path,    'tmp/run/www.pid'
+set :www_sock_path,   'tmp/run/www.sock'
 
 
-if ENV.key? 'DEPLOY_INIT'
-  ensure_dir app_path, 0701
-
-  #git_clone repository, app_path
-  task :app_dir_init do
-    condition { no_git? app_path }
-
-    sh "git clone --depth 1 #{repository} #{app_path}"
-
-    %w[data data/sources tmp tmp/run].each do |e|
-      mkdir File.join(app_path, e), 0700
-    end
-    sh "cd #{app_path} && chmod 701 tmp tmp/run"
-  end
-
-  bundle_install
-
-  task :db_config do
-    path = "#{app_path}/config/database.yml"
-    conf = <<-eoh
-default: &default
-  adapter: postgresql
-  encoding: unicode
-  pool: 5
-
-production:
-  <<: *default
-  database: #{env.target}
-    eoh
-
-    condition { no_file? path }
-
-    file_write path, conf
-  end
-
-  task :db_check do
-    condition { no_sh 'ON_ERROR_STOP=1 psql -l' }
-
-    echo 'FATAL: Database is not accessible'
-    sh 'false'
-  end
-
-  task :db_init do
-    condition { no_sh 'psql -l | grep -E "^ +%s"' % env.target }
-
-    sh "cd #{app_path} && bundle exec rake db:create db:migrate"
-  end
-
-  task :secrets_config do
-    path = "#{app_path}/config/secrets.yml"
-    conf = <<-eoh
-production:
-  secret_key_base: #{SecureRandom.hex(64)}
-    eoh
-
-    condition { no_file? path }
-
-    file_write path, conf
-  end
-
-  task :www_config do
-    conf = <<-eoh
-worker_processes  #{www_workers}
-preload_app       false
-pid               '#{www_pid_path}'
-listen            "\#{ENV['HOME']}/#{app_path}/#{www_sock_path}"
-    eoh
-
-    condition { no_file_contains www_config_path, conf }
-
-    file_write www_config_path, conf
-  end
-else
-  git_update app_path
-end
-
-bundle_install
-
-task :db_migrate do
-  condition { sh "cd #{app_path} && bundle exec rake db:migrate:status | grep -E '^ +down'" }
-
-  sh "cd #{app_path} && bundle exec rake db:migrate"
-end
-
-task :assets_update do
-  sh "cd #{app_path} && bundle exec rake assets:precompile"
-  sh "cd #{app_path} && chmod 711 public public/assets"
-  sh "cd #{app_path} && find public/assets -type d -exec chmod 711 {} \\;"
-  sh "cd #{app_path} && find public/assets -type f -exec chmod 644 {} \\;"
-end
-
-
-
-if ENV.key? 'DEPLOY_INIT'
-  task :www_start do
-    condition { no_file? [app_path, www_pid_path].join('/') }
-
-    sh "cd #{app_path} && bundle exec unicorn -c config/unicorn.rb -D"
-  end
-
-  task :app_start do
-    condition { no_sh 'tmux has-session -t app' }
-
-    sh "cd #{app_path} && tmux new -d -s app 'foreman start -c queue=1,worker=#{queue_workers}; zsh'"
-  end
-
-else
-  task :app_stop do
-    sh 'tmux kill-session -t app'
-  end
-
-  #task :www_reload do
-  #  sh "kill -HUP $(cat #{app_path}/#{www_pid_path})"
-  #end
-
-  task :www_stop do
-    condition { file? [app_path, www_pid_path].join('/') }
-
-    sh "kill -QUIT $(cat #{app_path}/#{www_pid_path}); sleep 1"
-  end
-
-  task :www_start do
-    condition { no_file? [app_path, www_pid_path].join('/') }
-
-    sh "cd #{app_path} && bundle exec unicorn -c config/unicorn.rb -D"
-  end
-
-  task :app_start do
-    sh "cd #{app_path} && tmux new -d -s app 'foreman start -c queue=1,worker=#{queue_workers}; zsh'"
-  end
-end
+deploy
