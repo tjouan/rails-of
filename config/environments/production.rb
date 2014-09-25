de5edd9d68aee982c8345169c05bd8e8eca5b027
Thibault Jouan <tj@a13.fr>
2014-09-25 15:13:27 +0000

    Remove show exceptions middleware from prod env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,6 +82,8 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
 
+  config.middleware.delete 'ActionDispatch::ShowExceptions'
+
   config.middleware.use ExceptionNotification::Rack,
     email: {
       email_prefix:         '[opti-front] ',
