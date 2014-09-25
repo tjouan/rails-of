ddffe0dd69219b80b7eafb16c0df06095b082eab
Thibault Jouan <tj@a13.fr>
2014-09-25 19:12:39 +0000

    Fix production middlewares:
      No need to delete any middleware, unicorn will use
    Rack::ShowExceptions, unless RACK_ENV is set to "deployment" or "none".

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,9 +82,6 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
 
-  config.middleware.delete 'ActionDispatch::ShowExceptions'
-  config.middleware.delete 'ActionDispatch::DebugExceptions'
-
   config.middleware.use ExceptionNotification::Rack,
     email: {
       email_prefix:         '[opti-front] ',
