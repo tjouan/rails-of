690c8d3114ed39437f82116a201a8805daab6f86
Thibault Jouan <tj@a13.fr>
2014-10-06 16:01:08 +0000

    Redirect home/index to dashboard when authenticated

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,7 @@ class HomeController < ApplicationController
   skip_before_filter :authenticate!
 
   def index
+    redirect_to dashboard_path and return if current_user?
     @user   = User.new
     @offers = OffersPresenter.new(Offer.visible)
   end
