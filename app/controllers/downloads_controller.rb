a2e3d93e884c58f3a5550ba746baa7a1c27cdc26
Thibault Jouan <tj@a13.fr>
2014-10-22 15:36:59 +0000

    Suffix downloaded sources with `.csv`

--- app/controllers/downloads_controller.rb
+++ app/controllers/downloads_controller.rb
@@ -1,6 +1,6 @@
 class DownloadsController < ApplicationController
   def show
     source = current_user.sources.find params[:source_id]
-    send_file source.path, type: 'text/csv', filename: source.file_name
+    send_file source.path, type: 'text/csv', filename: source.file_name << '.csv'
   end
 end
