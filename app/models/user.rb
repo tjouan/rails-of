c40134e935274ae3595387f12d5d59f374a838a9
Thibault Jouan <tj@a13.fr>
2014-10-23 14:28:54 +0000

    Destroy subscriptions with user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   has_secure_password
 
   has_many :sources
-  has_many :subscriptions
+  has_many :subscriptions, dependent: :destroy
   has_many :works
 
   validates :email,     presence: true, uniqueness: true
