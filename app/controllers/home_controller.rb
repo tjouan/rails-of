0550c13c96ab5325c271311d55be203961809092
Thibault Jouan <tj@a13.fr>
2014-11-01 17:38:23 +0000

    Revert "Redirect home/index to dashboard when authenticated"
    This reverts commit 690c8d3114ed39437f82116a201a8805daab6f86.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,6 @@ class HomeController < ApplicationController
   skip_before_filter :authenticate!
 
   def index
-    redirect_to dashboard_path and return if current_user?
     @user   = User.new
     @offers = OffersPresenter.new(Offer.visible.sorted)
   end
