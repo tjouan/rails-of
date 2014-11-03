a26b35f1c4d494937bf8912c98c508106ebae8db
Thibault Jouan <tj@a13.fr>
2014-11-03 21:41:17 +0000

    Improve exports separator handling

--- app/models/export.rb
+++ app/models/export.rb
@@ -1,5 +1,10 @@
 class Export < ActiveRecord::Base
-  CHARSETS = %w[utf-8 iso-8859-15 iso-8859-1].freeze
+  CHARSETS    = %w[utf-8 iso-8859-15 iso-8859-1].freeze
+  SEPARATORS  = {
+    ','   =>  'comma',
+    ';'   =>  'semicolon',
+    "\t"  =>  'tabulation'
+  }.freeze
 
   belongs_to :source
 
@@ -8,7 +13,7 @@ class Export < ActiveRecord::Base
   validates :source,    presence: true
   validates :charset,   presence: true
   validates :header,    presence: true
-  validates :separator, presence: true
+  validates :separator, inclusion: SEPARATORS.keys
   validates :file_name, presence: true
 
   default_scope { order('created_at DESC') }
@@ -24,6 +29,14 @@ class Export < ActiveRecord::Base
 
   def available_charsets
     CHARSETS
+  end
+
+  def separators
+    SEPARATORS
+  end
+
+  def separator_description
+    separator.inspect.gsub '"', ''
   end
 
   def set_defaults
